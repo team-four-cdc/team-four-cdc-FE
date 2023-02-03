c5e3232f25c7fcdf685b6c944937a9984ee5c99a
Muhammad Breda Taftayani <Muhammad_B782@mitrais.com>
2023-02-03 09:36:50 +0700

    #update tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'primary-color': '#3B3737',
+      'secondary-color': '#555050',
+      'monocrom-color': '#F5F5F5',
+      'success-color': '#5ABF41',
+      'danger-color': '#CA3144',
+      'warning-color': '#EDE642',
+    },
     extend: {},
+    borderRadius: {
+      'radius-10px': '10px',
+      'radius-20px': '20px',
+      'radius-100px': '100px',
+    },
   },
   plugins: [],
   corePlugins: {
