bdd5c959ceec9ce9b12d0c11267e9f91fd9a0c3d
Mitrais-Faisal <faisalnur.hakim@mitrais.com>
2023-02-03 18:22:19 +0700

    fix: fix conflict between antd, global css, and tailwind

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+const withLess = require('next-with-less');
+const lessToJS = require('less-vars-to-js');
+const fs = require('fs');
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-};
+const themeVariables = lessToJS(
+  fs.readFileSync(
+    path.resolve(__dirname, './src/styles/variables.less'),
+    'utf8'
+  )
+);
 
-module.exports = nextConfig;
+module.exports = withLess({
+  lessLoaderOptions: {
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: themeVariables,
+      localIdentName: '[path]___[local]___[hash:base64:5]',
+    },
+  },
+  reactStrictMode: true,
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import { store } from '@/store';
-import '@/styles/globals.scss';
+import '@/styles/index.less';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { store } from '@/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
