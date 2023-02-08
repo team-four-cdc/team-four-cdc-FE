fbaf77c5b8bc845c8b62a241f82e850888f7a638
Muhammad Breda Taftayani <Muhammad_B782@mitrais.com>
2023-02-08 10:06:52 +0700

    #update navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
-import { Typography } from 'antd';
+import { Dropdown, Menu, Typography } from 'antd';
 import Link from 'next/link';
 
 export default function Navbar() {
+  const menu = (
+    <Menu>
+      <Menu.Item>item 1</Menu.Item>
+      <Menu.Item>item 2</Menu.Item>
+    </Menu>
+  );
   return (
     <div className="flex flex-row bg-monocrom-color px-30px py-20px shadow-primary-box-shadow">
       <div className="p-20">
@@ -11,7 +17,9 @@ export default function Navbar() {
       </div>
       <div className="flex p-20px flex-row space-x-30px ml-auto">
         <Link href={'/'}>Lihat Artikel</Link>
-        <Link href={'/'}>Masuk Akun</Link>
+        <Dropdown overlay={menu}>
+          <a>Masuk Akun</a>
+        </Dropdown>
         <Link href={'/'}>Registrasi</Link>
       </div>
     </div>
