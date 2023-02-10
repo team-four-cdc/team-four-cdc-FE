91d64a11cd85cc1449df5aca18dbd7550cbaba00
winstonp-mitrais <124120022+winstonp-mitrais@users.noreply.github.com>
2023-02-10 09:31:51 +0700

    PAA-33: Button (#2)
    * Button element with color & style
    
    * Added JSDoc
    
    * Removed themeVariables and less-vars-to-js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 const withLess = require('next-with-less');
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
-
-const themeVariables = lessToJS(
-  fs.readFileSync(
-    path.resolve(__dirname, './src/styles/variables.less'),
-    'utf8'
-  )
-);
 
 module.exports = withLess({
   lessLoaderOptions: {
     lessOptions: {
       javascriptEnabled: true,
-      modifyVars: themeVariables,
+      //modifyVars: themeVariables,
       localIdentName: '[path]___[local]___[hash:base64:5]',
     },
   },
