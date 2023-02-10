26e6dddd9768165e81fe23d1bbb75c5ef02dd1a5
NataMitrais <124120315+NataMitrais@users.noreply.github.com>
2023-02-10 10:20:57 +0800

    CardArticle (#4)
    component Article Done
    
    Co-authored-by: Kresnata Adi Surya <kresnata17@gmail.com>

---
+++ src/components/CardArticlePreview.tsx
@@ -0,0 +1,36 @@
+import { Avatar, Card, CardProps, Typography } from 'antd';
+import React from 'react';
+
+interface StandardCardProps extends CardProps {
+  creator: string;
+  preview: string;
+  date: string;
+  avatar: string;
+}
+
+const { Meta } = Card;
+
+export default function CardArticlePreview(
+  props: StandardCardProps
+): JSX.Element {
+  return (
+    <div className="p-20">
+      <Card
+        {...props}
+        className="flex flex-row bg-monocrom-color shadow-primary-box-shadow rounded-radius-10px"
+      >
+        <Meta avatar={<Avatar src={props.avatar} />} title={props.creator} />
+        <div className="display-block pt-20px">
+          <Typography.Text className="text-18px !text-primary-color font-medium">
+            {props.preview}
+          </Typography.Text>
+        </div>
+        <div>
+          <Typography.Text className="text-12px !text-secondary-color">
+            {props.date}
+          </Typography.Text>
+        </div>
+      </Card>
+    </div>
+  );
+}
