2cdbc988a4c5209352bf3ac719322b139764c35b
winstonp-mitrais <winston.pingnomo@mitrais.com>
2023-02-14 14:53:05 +0700

    Unit test

---
+++ src/tests/pages/registrasi-pembaca.test.tsx
@@ -0,0 +1,12 @@
+import RegistrasiPembaca from '@/pages/registrasi-pembaca';
+import { render, screen } from '@testing-library/react';
+
+test('should render essential UI components', () => {
+  render(<RegistrasiPembaca />);
+  const emailField = screen.getByRole('textbox', { name: /email/i });
+  const passwordField = screen.getByPlaceholderText(/password/i);
+  const signupButton = screen.getByRole('button', { name: /daftarkan akun/i });
+  expect(emailField).toBeInTheDocument();
+  expect(passwordField).toBeInTheDocument();
+  expect(signupButton).toBeInTheDocument();
+});
