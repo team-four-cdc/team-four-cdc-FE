9be780ea8a0361c9bc8db31f75205f0a7df7397c
Mitrais-Faisal <faisalnur.hakim@mitrais.com>
2023-02-23 12:07:28 +0700

    feat: add redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,14 @@
 import { authApi, newsApi } from '@/services';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from '@/store/auth/authSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  storage,
+  key: 'root',
+  whitelist: ['auth'],
+};
 
 export const rootReducer = combineReducers({
   auth: authReducer,
@@ -8,14 +16,18 @@ export const rootReducer = combineReducers({
   [newsApi.reducerPath]: newsApi.reducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat([
       authApi.middleware,
       newsApi.middleware,
     ]),
 });
+
+export const persistor = persistStore(store);
 
 export type Store = typeof store;
 export type Reducer = typeof rootReducer;
