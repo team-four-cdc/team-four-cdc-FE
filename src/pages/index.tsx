c2305e571dea12139d3cfb41b0921658bdf055f6
Mitrais-Faisal <faisalnur.hakim@mitrais.com>
2023-03-01 12:26:10 +0700

    refactor: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,7 @@
 import Heads from '@/layout/Head/Head';
-import { useGetNewsQuery } from '@/services';
 import { Typography } from 'antd';
-import { useEffect } from 'react';
 
 export default function Home() {
-  const { data, isLoading, error } = useGetNewsQuery();
-
-  useEffect(() => {
-    console.log(data);
-    console.log(isLoading);
-    console.log(error);
-  }, [data, error, isLoading]);
-
   return (
     <>
       <Heads showNavbar={true} title="BacaAku" />
