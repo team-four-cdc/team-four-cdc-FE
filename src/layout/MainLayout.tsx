18f44eec68277b3cc95fbf2113136545e881b4f4
taftaya <muhammadbreda.taftayani@mitrais.com>
2023-05-09 09:14:56 +0700

    #update slicing homepage list penulis

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,14 @@
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function MainLayout({ children }: any) {
   const { auth } = useSelector((state: any) => state);
-  // const Router = useRouter();
+  const Router = useRouter();
   useEffect(() => {
-    // auth.role == 'creator' && auth.isLogin
-    //   ? Router.push('/dashboard-penulis')
-    //   : Router.push('/');
+    auth.role == 'creator' && auth.isLogin
+      ? Router.push('/dashboard-penulis')
+      : '';
   }, [auth]);
   return <>{children}</>;
 }
