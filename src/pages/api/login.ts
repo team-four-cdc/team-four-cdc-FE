b85a99b78f636c11a51f40cf86b5b23fba0994b5
chadwithz <chadwithz@email.com>
2023-06-25 18:19:15 +0700

    feat: add login api on FE

---
+++ src/pages/api/login.ts
@@ -0,0 +1,35 @@
+import { sessionOptions } from "@/data/sessionOptions";
+import { LoginResponse } from "@/services";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import jwt_decode from 'jwt-decode';
+import { DecodedToken, IUser } from "@/store/auth/authSlice";
+
+export default withIronSessionApiRoute(loginRoute, sessionOptions);
+
+async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
+  const { email, password, role = 'reader' } = await req.body;
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string
+  const reqData = {
+    email,
+    password
+  }
+  try {
+    const dataRaw = await axios.post(`${BASE_URL}/auth/login/${role}`, reqData)
+
+    const data = dataRaw.data as LoginResponse
+
+    const token = data.data.token
+
+    const auth = token ? jwt_decode<DecodedToken>(token) : {};
+
+    req.session.auth = { ...auth, token } as IUser
+
+    await req.session.save();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ message: (err as Error).message });
+  }
+}
+
---
+++ src/pages/api/user.ts
@@ -0,0 +1,23 @@
+import { sessionOptions } from "@/data/sessionOptions";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default withIronSessionApiRoute(userRoute, sessionOptions);
+
+async function userRoute(req: NextApiRequest, res: NextApiResponse) {
+  if (req.session.auth) {
+    // in a real world application you might read the user id from the session and then do a database request
+    // to get more information on the user if needed
+    res.json({
+      ...req.session.auth,
+      isLoggedIn: true,
+    });
+  } else {
+    res.json({
+      isLoggedIn: false,
+      login: "",
+      avatarUrl: "",
+    });
+  }
+}
+
